c1503b3dc3a6309db6ad098010d76e9ec5d3b27c
jamilalisgandarov <jamil.e@code.edu.az>
2019-07-06 14:45:05 +0400

    Initial

---
+++ assets/js/main.js
@@ -0,0 +1,138 @@
+// var classes = ["programming", '3ddesign', 'msoffice', 'sysadmin'];
+// var mix = ["programming", 10000, 'msoffice', 3000, true, false];
+
+// const person = {
+//   age: 21,
+//   height: 186,
+//   name: 'Jamil',
+//   isMarried: false,
+//   educations: [{
+//       name: 'Code Academy'
+//     },
+//     {
+//       name: 'ASOIU',
+//     },
+//     true
+//   ],
+//   '1': {
+//     name: 'Macbook PRO',
+//     releaseDate: '2018'
+//   }
+// };
+
+// person.name = 'John';
+
+// person = {};
+
+// console.log(person.name);
+
+// console.log(person['age']);
+// person.list
+// person['age']
+
+// console.log(person);
+
+// console.log('Person name:', person.name);
+// console.log('Person name:', person.education[0].name);
+// console.log('Deyishenin tipi:', typeof person.education[0]);
+
+// var news = [{
+//     date: '2019',
+//     list: [{
+//       title: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu!',
+//       desc: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu. İnstitutun 2017-ci ildən Azərbaycandakı rəsmi təhsil nümayəndəsi olan Code Academy iki illik uğurlu tərəfdaşlıq müddətində 100-dən çox tələbənin DMİ kurrikulumuna əsasən tədris almasına və “Certified Digital Marketing Professional”  beynəlxalq sertifikatını əldə etməsinə imkan yaratmışdır.',
+//       date: '04.06.2019',
+//       img: 'http://code.edu.az/Content/Images/f3c0df70-695c-4646-a74d-db32b88a6d0c_Slide-White.jpg',
+//     }]
+//   },
+//   {
+//     date: '2018',
+//     list: [{
+//         title: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu!',
+//         desc: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu. İnstitutun 2017-ci ildən Azərbaycandakı rəsmi təhsil nümayəndəsi olan Code Academy iki illik uğurlu tərəfdaşlıq müddətində 100-dən çox tələbənin DMİ kurrikulumuna əsasən tədris almasına və “Certified Digital Marketing Professional”  beynəlxalq sertifikatını əldə etməsinə imkan yaratmışdır.',
+//         date: '04.06.2019',
+//         img: 'http://code.edu.az/Content/Images/f3c0df70-695c-4646-a74d-db32b88a6d0c_Slide-White.jpg',
+//       },
+//       {
+//         title: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu!',
+//         desc: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu. İnstitutun 2017-ci ildən Azərbaycandakı rəsmi təhsil nümayəndəsi olan Code Academy iki illik uğurlu tərəfdaşlıq müddətində 100-dən çox tələbənin DMİ kurrikulumuna əsasən tədris almasına və “Certified Digital Marketing Professional”  beynəlxalq sertifikatını əldə etməsinə imkan yaratmışdır.',
+//         date: '04.06.2019',
+//         img: 'http://code.edu.az/Content/Images/f3c0df70-695c-4646-a74d-db32b88a6d0c_Slide-White.jpg',
+//       },
+//       {
+//         title: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu!',
+//         desc: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu. İnstitutun 2017-ci ildən Azərbaycandakı rəsmi təhsil nümayəndəsi olan Code Academy iki illik uğurlu tərəfdaşlıq müddətində 100-dən çox tələbənin DMİ kurrikulumuna əsasən tədris almasına və “Certified Digital Marketing Professional”  beynəlxalq sertifikatını əldə etməsinə imkan yaratmışdır.',
+//         date: '04.06.2019',
+//         img: 'http://code.edu.az/Content/Images/f3c0df70-695c-4646-a74d-db32b88a6d0c_Slide-White.jpg',
+//       },
+//       {
+//         title: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu!',
+//         desc: 'Code Academy Digital Marketinq İnstitutunun eksklüziv partnyoru oldu. İnstitutun 2017-ci ildən Azərbaycandakı rəsmi təhsil nümayəndəsi olan Code Academy iki illik uğurlu tərəfdaşlıq müddətində 100-dən çox tələbənin DMİ kurrikulumuna əsasən tədris almasına və “Certified Digital Marketing Professional”  beynəlxalq sertifikatını əldə etməsinə imkan yaratmışdır.',
+//         date: '04.06.2019',
+//         img: 'http://code.edu.az/Content/Images/f3c0df70-695c-4646-a74d-db32b88a6d0c_Slide-White.jpg',
+//       }
+//     ]
+//   }
+// ]
+
+// If else
+
+// console.log('a+b=', a + b);
+// console.log('a*b=', a * b);
+// console.log('a/b=', a / b);
+// console.log('a-b=', a - b);
+// console.log('a%b=', a % b);
+
+// console.log('a+12=', '12' + (a + b)); //12129
+
+// if (operator === '+') {
+//   console.log(a + b);
+// } else if (operator === '-') {
+//   console.log(a - b);
+// } else if (operator === '*') {
+//   console.log(a * b);
+// } else {
+//   console.log(a / b);
+// }
+
+// const students = [{
+//     name: 'John',
+//   },
+//   {
+//     name: 'Jamil',
+//   }
+// ];
+
+// const a = 12;
+// const b = 9;
+// const operator = '*';
+
+// switch (operator) {
+//   case '+':
+//     console.log(a + b);
+//     break;
+//   case "-":
+//     console.log(a - b);
+//     break;
+//   case "*":
+//     console.log(a * b);
+//     break;
+//   default:
+//     console.log(a / b);
+//     break;
+// }
+
+// true + '12' - false
+
+if (true) {
+  var c = 12;
+}
+
+if (true) {
+  const a = 12;
+  console.log('a value inside condition', a);
+}
+
+var a = '14';
+console.log('a value outside of condition', a);
+console.log(c);
